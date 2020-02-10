6b78e2dbbc436aab18681592901491620769b03c
Weerayut Hongsa <kusumoto.com@gmail.com>
2020-02-11 00:04:25 +0700

    Add react native module function

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import { NativeModules } from 'react-native';
 
 const { ReactNativeSwitchAudioOutput } = NativeModules;
 
-export default ReactNativeSwitchAudioOutput;
+export default class ReactNativeSwitchAudioOutput {
+    static isSpeakerphoneOn = cb => ReactNativeSwitchAudioOutput.isSpeakerphoneOn(cb)
+    static switchAudioOutput = (isSpeakerPhoneOn, cb) => ReactNativeSwitchAudioOutput.switchAudioOutput(isSpeakerPhoneOn, cb)
+}
+
