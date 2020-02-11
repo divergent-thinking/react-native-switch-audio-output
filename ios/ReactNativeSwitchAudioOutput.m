ab660d20a9a68b6ea64e931103435978dcb93455
Weerayut Hongsa <kusumoto.com@gmail.com>
2020-02-11 16:18:13 +0700

    add ios native code

--- ios/ReactNativeSwitchAudioOutput.m
+++ ios/ReactNativeSwitchAudioOutput.m
@@ -3,12 +3,27 @@
 
 @implementation ReactNativeSwitchAudioOutput
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+RCT_EXPORT_METHOD(switchAudioOutput: (BOOL *)isSpeckerPhoneOn) {
+  NSError* error;
+  AVAudioSession* session = [AVAudioSession sharedInstance];
+
+  [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
+  [session setMode:AVAudioSessionModeVoiceChat error:&error];
+  RCTLogInfo(@"Setting audio output");
+  if (isSpeckerPhoneOn) // Enable speaker
+  {
+    [session overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker error:&error];
+    RCTLogInfo(@"Enable Speaker");
+  }
+  else // Disable speaker
+  {
+    [session overrideOutputAudioPort:AVAudioSessionPortOverrideNone error:&error];
+    RCTLogInfo(@"Disable Speaker");
+  }
+  [session setActive:YES error:&error];
 }
 
 @end
+
