0ed44b51070379f49ebd5cb47f273b7bfccdb3f8
Weerayut Hongsa <kusumoto.com@gmail.com>
2020-02-15 01:36:18 +0700

    add ios script switch audio output

--- ios/ReactNativeSwitchAudioOutput.m
+++ ios/ReactNativeSwitchAudioOutput.m
@@ -1,14 +1,25 @@
 #import "ReactNativeSwitchAudioOutput.h"
-
+#import <AVFoundation/AVFoundation.h>
 
 @implementation ReactNativeSwitchAudioOutput
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+RCT_EXPORT_METHOD(switchAudioOutput: (BOOL *)isSpeckerPhoneOn) {
+  NSError* error;
+  AVAudioSession* session = [AVAudioSession sharedInstance];
+
+  [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
+  [session setMode:AVAudioSessionModeVoiceChat error:&error];
+  if (isSpeckerPhoneOn) // Enable speaker
+  {
+    [session overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker error:&error];
+  }
+  else // Disable speaker
+  {
+    [session overrideOutputAudioPort:AVAudioSessionPortOverrideNone error:&error];
+  }
+  [session setActive:YES error:&error];
 }
-
 @end
+
--- react-native-react-native-switch-audio-output.podspec
+++ react-native-react-native-switch-audio-output.podspec
@@ -12,7 +12,7 @@ Pod::Spec.new do |s|
   s.homepage     = "https://github.com/divergent-thinking/react-native-switch-audio-output"
   s.license      = "MIT"
   # s.license    = { :type => "MIT", :file => "FILE_LICENSE" }
-  s.authors      = { "Your Name" => "yourname@email.com" }
+  s.authors      = { "Your Name" => "kusumoto.com@gmail.com" }
   s.platforms    = { :ios => "9.0" }
   s.source       = { :git => "https://github.com/divergent-thinking/react-native-switch-audio-output.git", :tag => "#{s.version}" }
 
