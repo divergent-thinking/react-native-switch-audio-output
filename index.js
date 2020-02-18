54c84f9b952b2ec9c565ce6b2842ab3d17be375b
Weerayut Hongsa <kusumoto.com@gmail.com>
2020-02-18 17:06:33 +0700

    fix package conflict

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import { NativeModules } from 'react-native';
 
 const { ReactNativeSwitchAudioOutput } = NativeModules;
 
-export default class ReactNativeSwitchAudioOutput {
+export default class RNSwitchAudioOutput {
     static isSpeakerphoneOn = cb => ReactNativeSwitchAudioOutput.isSpeakerphoneOn(cb)
     static switchAudioOutput = (isSpeakerPhoneOn) => ReactNativeSwitchAudioOutput.switchAudioOutput(isSpeakerPhoneOn)
 }
-
