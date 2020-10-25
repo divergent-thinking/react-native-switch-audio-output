ca305c1fbba7f998f2b42d8cf93b26c4a3d30711
Weerayut Hongsa <kusumoto.com@gmail.com>
2020-10-25 21:27:21 +0700

    add new function for clear method calling

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
 const { ReactNativeSwitchAudioOutput } = NativeModules;
 
 export default class RNSwitchAudioOutput {
-    static isSpeakerphoneOn = cb => ReactNativeSwitchAudioOutput.isSpeakerphoneOn(cb)
-    static switchAudioOutput = (isSpeakerPhoneOn) => ReactNativeSwitchAudioOutput.switchAudioOutput(isSpeakerPhoneOn)
+  static AUDIO_SPEAKER = "SPEAKER";
+  static AUDIO_HEADPHONE = "HEADPHONE";
+
+  static isSpeakerphoneOn = (cb) =>
+    ReactNativeSwitchAudioOutput.isSpeakerphoneOn(cb);
+
+  /**
+   * @deprecated Please replace to function 'selectAudioOutput'
+   */
+  static switchAudioOutput = (isSpeakerPhoneOn) => {
+    console.warn(
+      "Function is deprecated, this function is remove next version soon."
+    );
+    ReactNativeSwitchAudioOutput.switchAudioOutput(isSpeakerPhoneOn);
+  };
+
+  static selectAudioOutput = (audioOutput) => {
+    switch (audioOutput) {
+      case AUDIO_SPEAKER:
+        ReactNativeSwitchAudioOutput.switchAudioOutput(true);
+        break;
+      case AUDIO_HEADPHONE:
+        ReactNativeSwitchAudioOutput.switchAudioOutput(false);
+        break;
+      default:
+        throw new Error(
+          "selectAudioOutput `audioOutput` must be only `AUDIO_SPEAKER` and `AUDIO_HEADPHONE` only"
+        );
+    }
+  };
 }
